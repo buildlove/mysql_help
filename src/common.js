1174529fd1429a776b9bddfe08cd37d2dd5f01a2
buildlove <564845354@qq.com>
2019-01-24 16:36:39 +0800

    添加getType

--- src/common.js
+++ src/common.js
@@ -70,6 +70,27 @@ function deepClone(data){
   return obj;
 }
 
+function getType(obj){
+  //tostring会返回对应不同的标签的构造函数
+  var toString = Object.prototype.toString;
+  var map = {
+     '[object Boolean]'  : 'boolean', 
+     '[object Number]'   : 'number', 
+     '[object String]'   : 'string', 
+     '[object Function]' : 'function', 
+     '[object Array]'    : 'array', 
+     '[object Date]'     : 'date', 
+     '[object RegExp]'   : 'regExp', 
+     '[object Undefined]': 'undefined',
+     '[object Null]'     : 'null', 
+     '[object Object]'   : 'object'
+ };
+ if(obj instanceof Element) {
+      return 'element';
+ }
+ return map[toString.call(obj)];
+}
+
 module.exports = {
   assignSqlArg: assignSqlArg,
   sortArg: sortArg,
