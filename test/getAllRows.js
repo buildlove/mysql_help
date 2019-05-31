a63c0cc99068feff0cbdcf13ebc7bef869a788a7
buildlove <564845354@qq.com>
2019-05-31 14:58:54 +0800

    v2测试版本

---
+++ test/getAllRows.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const router = require('koa-router')();
+const app = new Koa();
+const bodyParser = require('koa-bodyparser')
+const mysql_help = require('../index.js')
+
+mysql_help.config('jobs', {
+  "mysql": {
+    "host": "127.0.0.1",
+    "port": "3306",
+    "user": "root",
+    "password": "Lixiaoqi2468"
+  }
+}).then(function(config){
+})
+
+app.use(router.routes());
+app.use(bodyParser());
+
+router.post('/getAllRows', async function (ctx) {
+  let userTable = new mysql_help('user')
+  let sql = await userTable.getAllRowsSQL({userid: 1})
+  console.log(sql)
+});
+
+app.listen(3000);
